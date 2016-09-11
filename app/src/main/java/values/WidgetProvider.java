6c256a0df9128f66f6974be2d1ec95668e0e90b8
Sandeep Raveesh <crsandy@gmail.com>
2016-09-10 22:47:04 -0700

    cleaned widgets

---
+++ app/src/main/java/values/WidgetProvider.java
@@ -0,0 +1,46 @@
+package values;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.codepath.doit.R;
+import com.codepath.doit.activities.MainActivity;
+
+public class WidgetProvider extends AppWidgetProvider {
+    public static String EXTRA_WORD=
+            "com.commonsware.android.appwidget.lorem.WORD";
+
+    @Override
+    public void onUpdate(Context ctxt, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        for (int i=0; i<appWidgetIds.length; i++) {
+            Intent svcIntent = new Intent(ctxt, WidgetService.class);
+
+            svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            RemoteViews widget = new RemoteViews(ctxt.getPackageName(),
+                    R.layout.to_do_widget);
+
+            widget.setRemoteAdapter(appWidgetIds[i], R.id.to_do_widget,
+                    svcIntent);
+
+            Intent clickIntent = new Intent(ctxt, MainActivity.class);
+            PendingIntent clickPI = PendingIntent
+                    .getActivity(ctxt, 0,
+                            clickIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+
+            widget.setPendingIntentTemplate(R.id.to_do_widget, clickPI);
+
+            appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
+        }
+
+        super.onUpdate(ctxt, appWidgetManager, appWidgetIds);
+    }
+}
---
+++ app/src/main/java/values/WidgetViewsFactory.java
@@ -0,0 +1,88 @@
+package values;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.codepath.doit.R;
+import com.codepath.doit.models.Item;
+import com.codepath.doit.utils.DBUtils;
+
+import java.util.List;
+
+public class WidgetViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+    private static List<Item> items;
+    private Context ctxt=null;
+    private int appWidgetId;
+
+    public WidgetViewsFactory(Context ctxt, Intent intent) {
+        this.ctxt=ctxt;
+        appWidgetId=intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+        populate();
+    }
+
+    private static void populate() {
+        items = DBUtils.readAll();
+    }
+
+    @Override
+    public void onCreate() {
+        // no-op
+    }
+
+    @Override
+    public void onDestroy() {
+        // no-op
+    }
+
+    @Override
+    public int getCount() {
+        return(items.size());
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews row=new RemoteViews(ctxt.getPackageName(),
+                R.layout.row);
+
+        row.setTextViewText(android.R.id.text1, items.get(position).subject);
+
+        Intent i=new Intent();
+        Bundle extras=new Bundle();
+
+        extras.putString(WidgetProvider.EXTRA_WORD, items.get(position).subject);
+        i.putExtras(extras);
+        row.setOnClickFillInIntent(android.R.id.text1, i);
+
+        return(row);
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return(null);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return(1);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return(position);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return(true);
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        // no-op
+    }
+}
