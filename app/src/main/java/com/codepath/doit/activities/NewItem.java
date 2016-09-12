c223f21c569df81be747e94a0fc7b900b49206f4
Sandeep Raveesh <crsandy@gmail.com>
2016-09-12 10:14:57 -0700

    new item page

--- app/src/main/java/com/codepath/doit/activities/NewItem.java
+++ app/src/main/java/com/codepath/doit/activities/NewItem.java
@@ -1,13 +1,41 @@
 package com.codepath.doit.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.codepath.doit.R;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
-public class NewItem extends AppCompatActivity {
+import java.util.Calendar;
+
+public class NewItem extends AppCompatActivity implements View.OnClickListener,
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_new_item);
+//
+//        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.priority_array, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//        spinner.setSelection(1);
+//    }
+
+
+    private TextView timeTextView;
+    private TextView dateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +49,84 @@ public class NewItem extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setSelection(1);
+
+        // Find our View instances
+        timeTextView = (TextView)findViewById(R.id.tvDisplayTime);
+        dateTextView = (TextView)findViewById(R.id.tvDisplayDate);
+        ImageView timeButton = (ImageView)findViewById(R.id.imgTime);
+        ImageView dateButton = (ImageView)findViewById(R.id.imgDate);
+
+
+        // Show a timepicker when the timeButton is clicked
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                TimePickerDialog tpd = TimePickerDialog.newInstance(
+                        NewItem.this,
+                        now.get(Calendar.HOUR_OF_DAY),
+                        now.get(Calendar.MINUTE),
+                        false
+                );
+                tpd.setTitle("TimePicker Title");
+                tpd.setAccentColor(Color.parseColor("#9C27B0"));
+                tpd.show(getFragmentManager(), "Timepickerdialog");
+            }
+        });
+
+        // Show a datepicker when the dateButton is clicked
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        NewItem.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.setAccentColor(Color.parseColor("#9C27B0"));
+                dpd.setTitle("DatePicker Title");
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        TimePickerDialog tpd = (TimePickerDialog) getFragmentManager().findFragmentByTag("Timepickerdialog");
+        DatePickerDialog dpd = (DatePickerDialog) getFragmentManager().findFragmentByTag("Datepickerdialog");
+
+        if(tpd != null) tpd.setOnTimeSetListener(this);
+        if(dpd != null) dpd.setOnDateSetListener(this);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+        String hourString = hourOfDay < 10 ? "0"+hourOfDay : ""+hourOfDay;
+        String minuteString = minute < 10 ? "0"+minute : ""+minute;
+        String secondString = second < 10 ? "0"+second : ""+second;
+        String time = hourString+"h "+minuteString+"m";
+        timeTextView.setText(time);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = dayOfMonth+"/"+(++monthOfYear)+"/"+year;
+        dateTextView.setText(date);
+    }
+
+    public void clearDate(View view) {
+        dateTextView.setText("Date not set");
+    }
+
+    public void clearTime(View view) {
+        timeTextView.setText("Time not set");
     }
 }
--- app/src/main/java/values/WidgetViewsFactory.java
+++ app/src/main/java/values/WidgetViewsFactory.java
@@ -3,7 +3,9 @@ package values;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
@@ -51,14 +53,18 @@ public class WidgetViewsFactory implements RemoteViewsService.RemoteViewsFactory
         RemoteViews row=new RemoteViews(ctxt.getPackageName(),
                 R.layout.row);
 
-        row.setTextViewText(android.R.id.text1, items.get(position).subject);
+        row.setTextViewText(R.id.text100, items.get(position).subject);
+        row.setTextViewTextSize(R.id.text100, TypedValue.COMPLEX_UNIT_DIP, 18);
+        row.setTextColor(R.id.text100, Color.WHITE);
+        row.setInt(R.id.text100, "setBackgroundColor",
+                android.graphics.Color.BLACK);
 
         Intent i=new Intent();
         Bundle extras=new Bundle();
 
         extras.putString(WidgetProvider.EXTRA_WORD, items.get(position).subject);
         i.putExtras(extras);
-        row.setOnClickFillInIntent(android.R.id.text1, i);
+        row.setOnClickFillInIntent(R.id.text100, i);
 
         return(row);
     }
